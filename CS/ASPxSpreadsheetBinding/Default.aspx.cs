8f0ee67d49472bc6a4381bcb7b2bd70585d595f4
Ilya P <ilya.pavlov@devexpress.com>
2018-06-28 15:33:00 +0300

    add example content

---
+++ CS/ASPxSpreadsheetBinding/Default.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Data;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DevExpress.Spreadsheet;
+using DevExpress.Web.Office;
+
+namespace ASPxSpreadsheetSample {
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void OnSpreadsheetCallback(object sender, DevExpress.Web.CallbackEventArgsBase e)
+        {
+            Worksheet activeWorksheet = spreadsheet.Document.Worksheets.ActiveWorksheet;
+            activeWorksheet.SelectedCell.SetValueFromText(e.Parameter);
+        }
+    }
+}
